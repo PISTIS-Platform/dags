c3650fdd72be8eadd25cf30725e7d01fcb936d1a
IMR <ivan.martinez@eviden.com>
2025-10-02 12:10:37 +0200

    fix: fix typo near keyords

--- dags/pistis_workflow_dag.py
+++ dags/pistis_workflow_dag.py
@@ -135,7 +135,7 @@ from airflow.models import Variable
         "encryption": Param("Encryption Flag", type="string"),
         "periodicity": Param("Periodicity", type="string"),
         "dataset_category": Param("Category", type="string"),
-        "dataset_keywords":  Param("Keywords", type="string"),,
+        "dataset_keywords":  Param("Keywords", type="string"),
         "raw_wf": Param(
             [{
                 "prev_run": "000",
